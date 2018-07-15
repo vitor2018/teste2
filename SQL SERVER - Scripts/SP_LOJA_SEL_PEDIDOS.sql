3f5cc8af6eb4cbfa99a622e376bc7c19e90115ef
vitorazzone <vitor.azzone@outlook.com>
2018-07-15 17:45:23 -0300

    atualização 2

--- SQL SERVER - Scripts/SP_LOJA_SEL_PEDIDOS.sql
+++ SQL SERVER - Scripts/SP_LOJA_SEL_PEDIDOS.sql
@@ -1,35 +1,27 @@
-/*CREATE PROCEDURE SP_LOJA_SEL_PEDIDOS*/
-IF NOT EXISTS(SELECT * FROM SYS.DATABASES WHERE NAME = 'LOJA')
+IF OBJECT_ID('SP_LOJA_SEL_PEDIDOS') > 0
 BEGIN
-CREATE DATABASE [Loja]
-END
-USE Loja
-GO
-IF (OBJECT_ID('SP_LOJA_SEL_PEDIDOS')) > 0
-BEGIN
-	DROP PROCEDURE [SP_LOJA_SEL_PEDIDOS]
+	DROP PROCEDURE SP_LOJA_SEL_PEDIDOS
 END;
 GO
-
 CREATE PROCEDURE [dbo].[SP_LOJA_SEL_PEDIDOS]
 (
 	@ID_Cliente AS INT = 0,
 	@NR_Pedido AS INT = 0,
-	@DT_EntregaInicial AS DATETIME = '',
-	@DT_EntregaFinal AS DATETIME = ''
+	@DT_EntregaInicial AS CHAR(10) = '',
+	@DT_EntregaFinal AS CHAR(10) = ''
 )
 AS
 BEGIN
 	SELECT DISTINCT P.ID_Pedido,
 		C.NM_Cliente,
-		P.NR_Valor
+		P.NR_Valor		
 	FROM Pedidos P
 	LEFT JOIN Clientes C WITH(NOLOCK)
 		ON C.ID_Cliente = P.ID_Cliente
-	WHERE 
+	WHERE
 		(ISNULL(@NR_Pedido, 0) = 0 OR P.ID_Pedido = @NR_Pedido)
 		AND (ISNULL(@ID_Cliente, 0) = 0 OR P.ID_Cliente = @ID_Cliente)
-		AND ((CAST(CONVERT(VARCHAR, P.DT_Entrega, 103) AS Date) >= CAST(CONVERT(VARCHAR, @DT_EntregaInicial, 103) AS DATE) OR ISNULL(@DT_EntregaInicial, '') = '')
-		AND (CAST(CONVERT(VARCHAR, P.DT_Entrega, 103) AS DATE) <= CAST(CONVERT(VARCHAR, @DT_EntregaFinal, 103) AS DATE) OR ISNULL(@DT_EntregaFinal, '') = ''))		
+		AND (ISNULL(@DT_EntregaInicial, '') = '' OR CONVERT(DATE, @DT_EntregaInicial) <= CONVERT(DATE, P.DT_Entrega))
+		AND (ISNULL(@DT_EntregaFinal, '') = '' OR CONVERT(DATE, P.DT_Entrega) <= CONVERT(DATE, @DT_EntregaFinal))
 	ORDER BY C.NM_Cliente
-END
+END;
